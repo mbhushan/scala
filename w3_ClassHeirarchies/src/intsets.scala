bb0aa1eabe888da1348d13e54a9e96ccf3841cb6
manibhushan <manibhushan.cs@gmail.com>
2016-08-03 07:54:51 -0700

    binary tree basic class

---
+++ w3_ClassHeirarchies/src/intsets.scala
@@ -0,0 +1,40 @@
+
+object intsets {
+  def main(args: Array[String]): Unit = {
+    val t1 = new NonEmpty(3, new Empty, new Empty)
+     val t2 = t1 incl 4    
+     
+     println(t1);
+    println(t2);
+  }
+}
+
+abstract class IntSet {
+  def incl(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
+
+class Empty extends IntSet {
+  def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty);
+  def contains(x: Int): Boolean = false;
+  
+  override def toString = "." 
+}
+
+class NonEmpty(data: Int, left: IntSet, right: IntSet) extends IntSet {
+  
+  def contains(x: Int): Boolean = {
+    if (x < data) left contains x
+    else if (x > data) right contains x
+    else true
+  }
+  
+  def incl(x: Int): IntSet = {
+     if (x < data) new NonEmpty(data, left incl x, right)
+     else if (x > data) new NonEmpty(data, left, right incl x)
+     else this
+  }
+  
+  override def toString = "{" + left + data + right + "}"
+}
+
