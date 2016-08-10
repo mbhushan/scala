87700c21b967c198f89184e535972b16c2f2eb14
manibhushan <manibhushan.cs@gmail.com>
2016-08-10 06:53:39 -0700

    natural number class

---
+++ w4_NaturalNumbers/src/Nat.scala
@@ -0,0 +1,24 @@
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.IllegalArgumentException
+
+
+abstract class Nat {
+  def isZero: Boolean
+  def predecessor: Nat
+  def successor = new Succ(this)
+  def + (that: Nat): Nat
+  def - (that: Nat): Nat
+}
+
+object Zero extends Nat {
+  def isZero = true;
+  def predecessor = throw new Error("0.predecessor")
+  def +(that: Nat) = that
+  def -(that: Nat) = if (that.isZero) this else throw new Error("negative number")
+}
+
+class Succ(n: Nat) extends Nat {
+  def isZero = false
+  def predecessor = n
+  def +(that: Nat) = new Succ(n + that)
+  def -(that: Nat) = if (n.isZero) this else n - that.predecessor
+}
